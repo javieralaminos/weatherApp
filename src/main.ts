bceadd7085e75b6102b6d8a554049d9383780548
javieralaminos <javier.alaminos@erudit.ai>
2024-02-13 13:58:10 +0100

    feat: python back

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const table = new Table(this, 'weather', {
+      tableName: 'weather',
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    // Create the Lambda function
+    const lambdaFunction = new Function(this, 'weatherLambda', {
+      functionName: 'weatherLambda',
+      runtime: Runtime.PYTHON_3_10,
+      handler: 'server.handler',
+      code: Code.fromAsset('backend', {
+        bundling: {
+          image: Runtime.PYTHON_3_10.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
+          ],
+        },
+      }),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+    table.grantReadWriteData(lambdaFunction);
+    new LambdaRestApi(this, 'weatherApi', {
+      handler: lambdaFunction,
+    });
   }
 }
 
